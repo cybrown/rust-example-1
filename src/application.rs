6d5829f986b824ca2bc5cca3fbc36689142048c0
Cy Brown <cy.brown59@gmail.com>
2021-01-23 03:50:10 +0100

    hand crafted spies

--- src/application.rs
+++ src/application.rs
@@ -46,3 +46,85 @@ impl Application {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct LoggerMock {
+        pub called: RefCell<bool>,
+    }
+
+    impl LoggerMock {
+        fn new() -> Self {
+            Self {
+                called: RefCell::new(false),
+            }
+        }
+    }
+
+    impl Logger for LoggerMock {
+        fn log(&self, _: std::string::String) {
+            *self.called.borrow_mut() = true;
+        }
+    }
+
+    struct UppercaserMock {
+        pub called: RefCell<bool>,
+    }
+
+    impl UppercaserMock {
+        fn new() -> Self {
+            Self {
+                called: RefCell::new(false),
+            }
+        }
+    }
+
+    impl Uppercaser for UppercaserMock {
+        fn to_uppercase(&self, _: std::string::String) -> std::string::String {
+            *self.called.borrow_mut() = true;
+            String::from("A")
+        }
+    }
+
+    struct CounterMock {
+        pub increment_called: RefCell<bool>,
+        pub get_value_called: RefCell<bool>,
+    }
+
+    impl CounterMock {
+        fn new() -> Self {
+            Self {
+                increment_called: RefCell::new(false),
+                get_value_called: RefCell::new(false),
+            }
+        }
+    }
+
+    impl Counter for CounterMock {
+        fn increment(&self) {
+            *self.increment_called.borrow_mut() = true;
+        }
+        fn get_value(&self) -> i32 {
+            *self.get_value_called.borrow_mut() = true;
+            0
+        }
+    }
+
+    #[test]
+    fn test_run() {
+        let logger = Rc::new(LoggerMock::new());
+        let uppercaser = Rc::new(UppercaserMock::new());
+        let counter = Rc::new(CounterMock::new());
+        let app = Application::new(uppercaser.clone(), logger.clone(), counter.clone());
+
+        app.run();
+
+        assert_eq!(true, *logger.called.borrow());
+        assert_eq!(true, *uppercaser.called.borrow());
+        assert_eq!(true, *counter.increment_called.borrow());
+        assert_eq!(false, *counter.get_value_called.borrow());
+    }
+}
