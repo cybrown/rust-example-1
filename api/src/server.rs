ecab760f48ecfac9664b2851a074a3f4508dbd61
Cy Brown <cy.brown59@gmail.com>
2021-02-02 19:46:53 +0100

    small route refactor

--- api/src/server.rs
+++ api/src/server.rs
@@ -1,44 +1,44 @@
 use crate::PostController;
 use std::convert::Infallible;
-use warp::{delete, get, post, put, serve, Filter, Rejection, Reply};
+use warp::{
+    any, body::json, delete, get, hyper::StatusCode, path, path::end, post, put, query, serve,
+    Filter, Rejection, Reply,
+};
 
 async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
     dbg!(err);
-    Ok(warp::reply::with_status(
-        warp::reply(),
-        warp::http::StatusCode::INTERNAL_SERVER_ERROR,
-    ))
+    Ok(StatusCode::INTERNAL_SERVER_ERROR)
 }
 
 pub async fn run_server(post_controller: PostController) {
     let posts_api = {
-        let with_post_controller = { warp::any().map(move || post_controller.clone()) };
+        let with_post_controller = { any().map(move || post_controller.clone()) };
 
         let get_posts = get()
             .and(with_post_controller.clone())
-            .and(warp::path::end())
-            .and(warp::query())
+            .and(end())
+            .and(query())
             .and_then(PostController::get_posts);
 
         let create_post = post()
             .and(with_post_controller.clone())
-            .and(warp::body::json())
-            .and(warp::path::end())
+            .and(json())
+            .and(end())
             .and_then(PostController::create_post);
 
         let get_post = get()
             .and(with_post_controller.clone())
-            .and(warp::path!(i32))
+            .and(path!(i32))
             .and_then(PostController::get_post);
 
         let publish_post = put()
             .and(with_post_controller.clone())
-            .and(warp::path!(i32 / "published"))
+            .and(path!(i32 / "published"))
             .and_then(PostController::publish_post);
 
         let unpublish_post = delete()
             .and(with_post_controller.clone())
-            .and(warp::path!(i32 / "published"))
+            .and(path!(i32 / "published"))
             .and_then(PostController::unpublish_post);
 
         warp::path("posts").and(
